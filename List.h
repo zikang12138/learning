d71872a4c0c1f9daf8662969b25a9f3cfad4dc40
zikang12138 <40554745+zikang12138@users.noreply.github.com>
2020-11-21 21:37:02 +0800

    Update List.h

--- List.h
+++ List.h
@@ -5,13 +5,13 @@ typedef sturct LNode{//
 }LNode *LinkList;
 
 bool find_kth(LinkList L,int k）{
-  LNode* p=L->next;
+  LNode* p=L->next;//p q都指示第一个节点
   LNode* q=L->next;
   int count=0;
   while(p!=nullptr)
   {
-    if(count<k)count++;
-    else q=q->next;
+    if(count<k)count++;//计数 如果count<k只移动p
+    else q=q->next； //如果count=k 则p q同时移动
     p=p->next;
   }
   if(count<k)
