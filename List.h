bf853f09cfb9b2434990179e4e073c3be53eb8aa
zikang12138 <40554745+zikang12138@users.noreply.github.com>
2020-11-21 21:35:03 +0800

    Update List.h

--- List.h
+++ List.h
@@ -1,7 +1,22 @@
-
+typedef int ElemType;
 typedef sturct LNode{//
-  int data;
+  ElemType data;
   struct LNode *next;
 }LNode *LinkList;
 
-
+bool find_kth(LinkList L,int k）{
+  LNode* p=L->next;
+  LNode* q=L->next;
+  int count=0;
+  while(p!=nullptr)
+  {
+    if(count<k)count++;
+    else q=q->next;
+    p=p->next;
+  }
+  if(count<k)
+    return false;
+  else{
+    cout<<q->data;
+    return true;}
+}
